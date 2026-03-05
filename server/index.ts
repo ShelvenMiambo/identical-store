16129befc65a98ab791326a3b1ce508906919a86
Shelven <shelvenmarlonnaftal43@gmail.com>
2026-03-05 22:35:14 +0200

    fix: increase body size limit to 25mb to allow image uploads

--- server/index.ts
+++ server/index.ts
@@ -17,11 +17,12 @@ declare module 'http' {
   }
 }
 app.use(express.json({
+  limit: '25mb',
   verify: (req, _res, buf) => {
     req.rawBody = buf;
   }
 }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '25mb' }));
 
 // Serve static files from attached_assets
 
